abe65845e6ff637fdc1df6ef32e276de37f87e9e
michaelmurwayi <https://github.com/michaelmurwayi>
2020-08-02 12:16:44 +0300

    store: editing store product holders

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import HomeView, SignupView, CartView, CheckoutView
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
+    path("", HomeView.as_view(), name="store"),
     path("cart/", CartView.as_view(), name="cart"),
     path("checkout/", CheckoutView.as_view(), name="checkout"),
     path("registration/", SignupView.as_view(), name="registration"),
